6b42f6f0ddd161b0f1e7cdaf3b65e47793fd392b
Destiny Boone <destaj.db@gmail.com>
2022-10-14 13:45:52 -0400

    singleton and factory patterns

--- app.js
+++ app.js
@@ -48,3 +48,76 @@ const ItemCtrl = (function(){
 
 ItemCtrl.add({id:1, name: 'John'});
 console.log(ItemCtrl.get(1));
+
+// SINGLETON PATTERN
+const Singleton = (function(){
+    let instance;
+
+    function createInstance(){
+        const object = new Object({name: 'Brad'});
+        return object;
+    }
+    return{
+        getInstance: function(){
+            if(!instance){
+                instance = createInstance();
+            }
+            return instance;
+        }
+    }
+})();
+
+const instanceA = Singleton.getInstance(); // only creates one instance no matter how many times you call
+const instanceB = Singleton.getInstance();
+
+console.log(instanceA === instanceB); // true
+
+// FACTORY PATTERN - used to create multiple objects
+function MemberFactory(){
+    this.createMember = function(name, type){
+        let member;
+
+        if(type === 'simple'){
+            member = new SimpleMembership(name);
+        } else if (type === 'standard') {
+            member = new StandardMembership(name);
+        } else if (type === 'super') {
+            member = new SuperMembership(name);
+        }
+
+        member.type = type;
+
+        member.define = function(){
+            console.log(`${this.name} (${this.type}): ${this.cost}`);
+        }
+
+        return member;
+    }
+}
+
+const SimpleMembership = function(name){
+    this.name = name;;
+    this.cost = '$5';
+}
+
+const StandardMembership = function(name){
+    this.name = name;;
+    this.cost = '$15';
+}
+
+const SuperMembership = function(name){
+    this.name = name;;
+    this.cost = '$25';
+}
+
+const members = [];
+const factory = new MemberFactory();
+
+members.push(factory.createMember('John Doe', 'simple'));
+members.push(factory.createMember('Chris Jackson', 'super'));
+members.push(factory.createMember('Janice Williams', 'simple'));
+members.push(factory.createMember('Tom Smith', 'standard'));
+
+members.forEach(function(member){
+    member.define();
+});
